9436f88b81c5751a7c1dc68f5565fa69cdabc4b9
Davis Hunter <108430642+kingarthurdavis78@users.noreply.github.com>
2023-05-23 16:28:33 -0600

    finish javadoc

--- src/main/java/service/EventService.java
+++ src/main/java/service/EventService.java
@@ -1,9 +1,19 @@
 package service;
 
+import result.EventResponse;
+
 /**
  * EventService Object
  */
 public class EventService {
+
+    /**
+     * Returns ALL events for ALL family members of the current user. The current user is determined from the provided auth token.
+     */
+    public EventResponse event() {
+        return null;
+    }
+
     /**
      * The EventService constructor
      */
